14d47c4a511e86bd0940156b3c92ee1ba1033fbb
zoinkgamergd-create <zoinkgamergd@gmail.com>
2026-07-16 22:20:59 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,98 +6,113 @@
 
 using namespace geode::prelude;
 
-// Función optimizada para reproducir y reutilizar las animaciones
-void playVideoAnimation(const std::string& animName, GJBaseGameLayer* layer) {
-    const int VIDEO_TAG = 80085;
-
-    auto animation = CCAnimationCache::sharedAnimationCache()->animationByName(animName.c_str());
-    if (!animation) return;
-
-    if (auto sprite = static_cast<CCSprite*>(layer->getChildByTag(VIDEO_TAG))) {
-        sprite->stopAllActions(); 
-        
-        auto animate = CCAnimate::create(animation);
-        auto sequence = CCSequence::create(
-            animate,
-            CCCallFunc::create(sprite, callfunc_selector(CCNode::removeFromParent)),
-            nullptr
-        );
-        sprite->runAction(sequence); 
-    } 
-    else {
-        std::string firstFrameName = "p1_frame_00.png";
-        if (animName == "p2_anim") firstFrameName = "p2_frame_00.png";
-        else if (animName == "dual_anim") firstFrameName = "dual_frame_00.png";
-
-        auto texture = CCTextureCache::sharedTextureCache()->textureForKey(firstFrameName.c_str());
-        if (!texture) return;
-
-        auto rect = CCRect{ 0, 0, texture->getContentSize().width, texture->getContentSize().height };
-        auto newSprite = CCSprite::createWithTexture(texture, rect);
-        newSprite->setTag(VIDEO_TAG);
-
-        auto winSize = CCDirector::sharedDirector()->getWinSize();
-        newSprite->setPosition(winSize / 2);
-        
-        float scaleX = winSize.width / newSprite->getContentSize().width;
-        float scaleY = winSize.height / newSprite->getContentSize().height;
-        newSprite->setScale(std::max(scaleX, scaleY));
-
-        auto animate = CCAnimate::create(animation);
-        auto sequence = CCSequence::create(
-            animate,
-            CCCallFunc::create(newSprite, callfunc_selector(CCNode::removeFromParent)),
-            nullptr
-        );
-
-        newSprite->runAction(sequence);
-        layer->addChild(newSprite, 9999);
-    }
-}
-
 class $modify(MyBaseGameLayer, GJBaseGameLayer) {
     struct Fields {
-        std::chrono::steady_clock::time_point m_lastP1Click{};
-        std::chrono::steady_clock::time_point m_lastP2Click{};
+        std::chrono::steady_clock::time_point m_lastP1Click = std::chrono::steady_clock::now() - std::chrono::hours(1);
+        std::chrono::steady_clock::time_point m_lastP2Click = std::chrono::steady_clock::now() - std::chrono::hours(1);
         bool m_initialized = false;
     };
+
+    // Método seguro y nativo para eliminar el sprite al terminar
+    void removeAnimSprite(CCNode* sender) {
+        if (sender) {
+            sender->removeFromParentAndCleanup(true);
+        }
+    }
+
+    // Reproducción optimizada integrada directamente en la clase
+    void playVideoAnimation(const std::string& animName) {
+        const int VIDEO_TAG = 80085;
+
+        auto animation = CCAnimationCache::get()->animationByName(animName.c_str());
+        if (!animation) return;
+
+        auto animate = CCAnimate::create(animation);
+
+        if (auto sprite = static_cast<CCSprite*>(this->getChildByTag(VIDEO_TAG))) {
+            sprite->stopAllActions(); 
+            auto sequence = CCSequence::create(
+                animate,
+                CCCallFuncN::create(this, callfuncN_selector(MyBaseGameLayer::removeAnimSprite)),
+                nullptr
+            );
+            sprite->runAction(sequence); 
+        } 
+        else {
+            std::string firstFrameName = "p1_frame_00.png";
+            if (animName == "p2_anim") firstFrameName = "p2_frame_00.png";
+            else if (animName == "dual_anim") firstFrameName = "dual_frame_00.png";
+
+            // Intentamos obtener el frame de la caché de Geode o del disco si hace falta
+            CCSpriteFrame* frame = CCSpriteFrameCache::get()->spriteFrameByName(firstFrameName.c_str());
+            if (!frame) {
+                auto texture = CCTextureCache::get()->textureForKey(firstFrameName.c_str());
+                if (texture) {
+                    auto rect = CCRect{ 0, 0, texture->getContentSize().width, texture->getContentSize().height };
+                    frame = CCSpriteFrame::createWithTexture(texture, rect);
+                }
+            }
+
+            if (!frame) return;
+
+            auto newSprite = CCSprite::createWithSpriteFrame(frame);
+            newSprite->setTag(VIDEO_TAG);
+
+            auto winSize = CCDirector::get()->getWinSize();
+            newSprite->setPosition(winSize / 2);
+            
+            float scaleX = winSize.width / newSprite->getContentSize().width;
+            float scaleY = winSize.height / newSprite->getContentSize().height;
+            newSprite->setScale(std::max(scaleX, scaleY));
+
+            auto sequence = CCSequence::create(
+                animate,
+                CCCallFuncN::create(this, callfuncN_selector(MyBaseGameLayer::removeAnimSprite)),
+                nullptr
+            );
+
+            newSprite->runAction(sequence);
+            this->addChild(newSprite, 9999);
+        }
+    }
 
     void pushButton(PlayerButton btn, bool isPlayer2) {
         GJBaseGameLayer::pushButton(btn, isPlayer2);
         
         if (btn != PlayerButton::Jump) return;
 
-        // Inicialización limpia compatible con cualquier compilador de Geode v3
+        // Inicialización con doble vía de carga (Caché de Geode o imágenes sueltas)
         if (!m_fields->m_initialized) {
             m_fields->m_initialized = true;
 
-            for (int i = 0; i < 30; i++) {
-                std::string num = (i < 10) ? "0" + std::to_string(i) : std::to_string(i);
-                
-                std::string p1File = "p1_frame_" + num + ".png";
-                std::string p2File = "p2_frame_" + num + ".png";
-                std::string dualFile = "dual_frame_" + num + ".png";
-
-                CCTextureCache::sharedTextureCache()->addImage(p1File.c_str());
-                CCTextureCache::sharedTextureCache()->addImage(p2File.c_str());
-                CCTextureCache::sharedTextureCache()->addImage(dualFile.c_str());
-            }
-
             auto cacheAnimation = [](const std::string& prefix, const std::string& animName) {
                 auto animFrames = CCArray::create();
+                auto textureCache = CCTextureCache::get();
+                auto frameCache = CCSpriteFrameCache::get();
+
                 for (int i = 0; i < 30; i++) {
                     std::string num = (i < 10) ? "0" + std::to_string(i) : std::to_string(i);
                     std::string fileName = prefix + "_" + num + ".png";
                     
-                    auto texture = CCTextureCache::sharedTextureCache()->textureForKey(fileName.c_str());
-                    if (texture) {
-                        auto rect = CCRect{ 0, 0, texture->getContentSize().width, texture->getContentSize().height };
-                        auto frame = CCSpriteFrame::createWithTexture(texture, rect);
+                    auto frame = frameCache->spriteFrameByName(fileName.c_str());
+                    if (!frame && textureCache) {
+                        auto texture = textureCache->addImage(fileName.c_str());
+                        if (texture) {
+                            auto rect = CCRect{ 0, 0, texture->getContentSize().width, texture->getContentSize().height };
+                            frame = CCSpriteFrame::createWithTexture(texture, rect);
+                            frameCache->addSpriteFrame(frame, fileName.c_str());
+                        }
+                    }
+                    
+                    if (frame) {
                         animFrames->addObject(frame);
                     }
                 }
-                auto animation = CCAnimation::createWithSpriteFrames(animFrames, 1.0f / 30.0f);
-                CCAnimationCache::sharedAnimationCache()->addAnimation(animation, animName.c_str());
+                
+                if (animFrames->count() > 0) {
+                    auto animation = CCAnimation::createWithSpriteFrames(animFrames, 1.0f / 30.0f);
+                    CCAnimationCache::get()->addAnimation(animation, animName.c_str());
+                }
             };
 
             cacheAnimation("p1_frame", "p1_anim");
@@ -119,9 +134,9 @@ class $modify(MyBaseGameLayer, GJBaseGameLayer) {
                 auto timeDiff = std::chrono::duration_cast<std::chrono::milliseconds>(now - m_fields->m_lastP2Click);
                 
                 if (timeDiff < simultaneousThreshold) {
-                    playVideoAnimation("dual_anim", this);
+                    this->playVideoAnimation("dual_anim");
                 } else {
-                    playVideoAnimation("p1_anim", this);
+                    this->playVideoAnimation("p1_anim");
                 }
             } 
             else {
@@ -129,17 +144,17 @@ class $modify(MyBaseGameLayer, GJBaseGameLayer) {
                 auto timeDiff = std::chrono::duration_cast<std::chrono::milliseconds>(now - m_fields->m_lastP1Click);
                 
                 if (timeDiff < simultaneousThreshold) {
-                    playVideoAnimation("dual_anim", this);
+                    this->playVideoAnimation("dual_anim");
                 } else {
-                    playVideoAnimation("p2_anim", this);
+                    this->playVideoAnimation("p2_anim");
                 }
             }
         } 
         else {
             if (!isPlayer2) {
-                playVideoAnimation("p1_anim", this);
+                this->playVideoAnimation("p1_anim");
             } else {
-                playVideoAnimation("p2_anim", this);
+                this->playVideoAnimation("p2_anim");
             }
         }
     }
