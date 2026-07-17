acc51da254c1f604256352607d60ea79006acb23
zoinkgamergd-create <zoinkgamergd@gmail.com>
2026-07-16 22:38:08 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,17 +13,12 @@ class $modify(MyBaseGameLayer, GJBaseGameLayer) {
         bool m_initialized = false;
     };
 
-    // Método seguro y nativo para eliminar el sprite al terminar
     void removeAnimSprite(CCNode* sender) {
-        if (sender) {
-            sender->removeFromParentAndCleanup(true);
-        }
+        if (sender) sender->removeFromParentAndCleanup(true);
     }
 
-    // Reproducción optimizada integrada directamente en la clase
     void playVideoAnimation(const std::string& animName) {
         const int VIDEO_TAG = 80085;
-
         auto animation = CCAnimationCache::get()->animationByName(animName.c_str());
         if (!animation) return;
 
@@ -31,131 +26,53 @@ class $modify(MyBaseGameLayer, GJBaseGameLayer) {
 
         if (auto sprite = static_cast<CCSprite*>(this->getChildByTag(VIDEO_TAG))) {
             sprite->stopAllActions(); 
-            auto sequence = CCSequence::create(
-                animate,
-                CCCallFuncN::create(this, callfuncN_selector(MyBaseGameLayer::removeAnimSprite)),
-                nullptr
-            );
-            sprite->runAction(sequence); 
-        } 
-        else {
-            std::string firstFrameName = "p1_frame_00.png";
-            if (animName == "p2_anim") firstFrameName = "p2_frame_00.png";
-            else if (animName == "dual_anim") firstFrameName = "dual_frame_00.png";
-
-            // Intentamos obtener el frame de la caché de Geode o del disco si hace falta
+            sprite->runAction(CCSequence::create(animate, CCCallFuncN::create(this, callfuncN_selector(MyBaseGameLayer::removeAnimSprite)), nullptr)); 
+        } else {
+            std::string firstFrameName = (animName == "p2_anim") ? "p2_frame_00.png" : ((animName == "dual_anim") ? "dual_frame_00.png" : "p1_frame_00.png");
             CCSpriteFrame* frame = CCSpriteFrameCache::get()->spriteFrameByName(firstFrameName.c_str());
-            if (!frame) {
-                auto texture = CCTextureCache::get()->textureForKey(firstFrameName.c_str());
-                if (texture) {
-                    auto rect = CCRect{ 0, 0, texture->getContentSize().width, texture->getContentSize().height };
-                    frame = CCSpriteFrame::createWithTexture(texture, rect);
-                }
-            }
-
             if (!frame) return;
 
             auto newSprite = CCSprite::createWithSpriteFrame(frame);
             newSprite->setTag(VIDEO_TAG);
-
             auto winSize = CCDirector::get()->getWinSize();
             newSprite->setPosition(winSize / 2);
-            
-            float scaleX = winSize.width / newSprite->getContentSize().width;
-            float scaleY = winSize.height / newSprite->getContentSize().height;
-            newSprite->setScale(std::max(scaleX, scaleY));
-
-            auto sequence = CCSequence::create(
-                animate,
-                CCCallFuncN::create(this, callfuncN_selector(MyBaseGameLayer::removeAnimSprite)),
-                nullptr
-            );
-
-            newSprite->runAction(sequence);
+            newSprite->setScale(std::max(winSize.width / newSprite->getContentSize().width, winSize.height / newSprite->getContentSize().height));
+            newSprite->runAction(CCSequence::create(animate, CCCallFuncN::create(this, callfuncN_selector(MyBaseGameLayer::removeAnimSprite)), nullptr));
             this->addChild(newSprite, 9999);
         }
     }
 
     void pushButton(PlayerButton btn, bool isPlayer2) {
         GJBaseGameLayer::pushButton(btn, isPlayer2);
-        
         if (btn != PlayerButton::Jump) return;
 
-        // Inicialización con doble vía de carga (Caché de Geode o imágenes sueltas)
         if (!m_fields->m_initialized) {
             m_fields->m_initialized = true;
-
             auto cacheAnimation = [](const std::string& prefix, const std::string& animName) {
                 auto animFrames = CCArray::create();
-                auto textureCache = CCTextureCache::get();
-                auto frameCache = CCSpriteFrameCache::get();
-
                 for (int i = 0; i < 30; i++) {
-                    std::string num = (i < 10) ? "0" + std::to_string(i) : std::to_string(i);
-                    std::string fileName = prefix + "_" + num + ".png";
-                    
-                    auto frame = frameCache->spriteFrameByName(fileName.c_str());
-                    if (!frame && textureCache) {
-                        auto texture = textureCache->addImage(fileName.c_str());
-                        if (texture) {
-                            auto rect = CCRect{ 0, 0, texture->getContentSize().width, texture->getContentSize().height };
-                            frame = CCSpriteFrame::createWithTexture(texture, rect);
-                            frameCache->addSpriteFrame(frame, fileName.c_str());
-                        }
-                    }
-                    
-                    if (frame) {
-                        animFrames->addObject(frame);
-                    }
+                    std::string fileName = prefix + "_" + ((i < 10) ? "0" + std::to_string(i) : std::to_string(i)) + ".png";
+                    if (auto frame = CCSpriteFrameCache::get()->spriteFrameByName(fileName.c_str())) animFrames->addObject(frame);
                 }
-                
-                if (animFrames->count() > 0) {
-                    auto animation = CCAnimation::createWithSpriteFrames(animFrames, 1.0f / 30.0f);
-                    CCAnimationCache::get()->addAnimation(animation, animName.c_str());
-                }
+                if (animFrames->count() > 0) CCAnimationCache::get()->addAnimation(CCAnimation::createWithSpriteFrames(animFrames, 1.0f / 30.0f), animName.c_str());
             };
-
             cacheAnimation("p1_frame", "p1_anim");
             cacheAnimation("p2_frame", "p2_anim");
             cacheAnimation("dual_frame", "dual_anim");
         }
 
-        bool isTwoPlayerLevel = false;
-        if (m_levelSettings) {
-            isTwoPlayerLevel = m_levelSettings->m_twoPlayerMode;
-        }
-
-        if (isTwoPlayerLevel) {
-            auto now = std::chrono::steady_clock::now();
-            const auto simultaneousThreshold = std::chrono::milliseconds(50);
-
+        bool isTwoPlayer = m_levelSettings && m_levelSettings->m_twoPlayerMode;
+        auto now = std::chrono::steady_clock::now();
+        if (isTwoPlayer) {
             if (!isPlayer2) {
                 m_fields->m_lastP1Click = now;
-                auto timeDiff = std::chrono::duration_cast<std::chrono::milliseconds>(now - m_fields->m_lastP2Click);
-                
-                if (timeDiff < simultaneousThreshold) {
-                    this->playVideoAnimation("dual_anim");
-                } else {
-                    this->playVideoAnimation("p1_anim");
-                }
-            } 
-            else {
-                m_fields->m_lastP2Click = now;
-                auto timeDiff = std::chrono::duration_cast<std::chrono::milliseconds>(now - m_fields->m_lastP1Click);
-                
-                if (timeDiff < simultaneousThreshold) {
-                    this->playVideoAnimation("dual_anim");
-                } else {
-                    this->playVideoAnimation("p2_anim");
-                }
-            }
-        } 
-        else {
-            if (!isPlayer2) {
-                this->playVideoAnimation("p1_anim");
+                this->playVideoAnimation((std::chrono::duration_cast<std::chrono::milliseconds>(now - m_fields->m_lastP2Click) < std::chrono::milliseconds(50)) ? "dual_anim" : "p1_anim");
             } else {
-                this->playVideoAnimation("p2_anim");
+                m_fields->m_lastP2Click = now;
+                this->playVideoAnimation((std::chrono::duration_cast<std::chrono::milliseconds>(now - m_fields->m_lastP1Click) < std::chrono::milliseconds(50)) ? "dual_anim" : "p2_anim");
             }
+        } else {
+            this->playVideoAnimation(isPlayer2 ? "p2_anim" : "p1_anim");
         }
     }
 };
